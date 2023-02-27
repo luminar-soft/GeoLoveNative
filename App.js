798816e2922deb1c8e8910911fd5afec6f1fa41c
rubanwd <126295482+luminar-soft@users.noreply.github.com>
2023-02-27 22:38:18 +0300

    test 1

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   console.log('test');
   console.log('122222222');
+  console.log('333333333');
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! 000000</Text>
